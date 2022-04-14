7e720d2ecf71bef9629c4b1dea47159dab40f8e8
harinadh76 <tannaharinadh@gmail.com>
2022-04-14 17:36:22 +0530

    drum-kit

---
+++ index.js
@@ -0,0 +1,54 @@
+
+function water() {
+  // alert("hello");
+  var audio = new Audio("sounds/crash.mp3");
+  audio.play();
+
+}
+
+
+function a() {
+  // alert("hello");
+  var audio = new Audio("sounds/kick-bass.mp3");
+  audio.play();
+
+}
+
+
+function s() {
+  // alert("hello");
+  var audio = new Audio("sounds/snare.mp3");
+  audio.play();
+
+}
+
+
+function d() {
+  // alert("hello");
+  var audio = new Audio("sounds/tom-1.mp3");
+  audio.play();
+
+}
+
+
+function j() {
+  // alert("hello");
+  var audio = new Audio("sounds/tom-2.mp3");
+  audio.play();
+
+}
+
+
+function k() {
+  // alert("hello");
+  var audio = new Audio("sounds/tom-3.mp3");
+  audio.play();
+
+}
+
+function l() {
+  // alert("hello");
+  var audio = new Audio("sounds/tom-4.mp3");
+  audio.play();
+
+}
